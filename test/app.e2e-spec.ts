8b6fca80aa782c3fdb61f113a13aa92a2699ef67
Cesar <csarfau@gmail.com>
2025-12-15 21:24:13 -0300

    feat: add e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from '../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('Transaction API (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
@@ -13,13 +13,67 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/transactions (POST) - should create a transaction', () => {
     return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .post('/transactions')
+      .send({
+        amount: 50.0,
+        timestamp: new Date().toISOString(),
+      })
+      .expect(201);
+  });
+
+  it('/transactions (POST) - should reject future date', () => {
+    const futureDate = new Date();
+    futureDate.setHours(futureDate.getHours() + 1);
+
+    return request(app.getHttpServer())
+      .post('/transactions')
+      .send({
+        amount: 50.0,
+        timestamp: futureDate.toISOString(),
+      })
+      .expect(422);
+  });
+
+  it('/statistics (GET) - should return stats', async () => {
+    await request(app.getHttpServer()).delete('/transactions').expect(200);
+
+    await request(app.getHttpServer())
+      .post('/transactions')
+      .send({ amount: 100, timestamp: new Date().toISOString() })
+      .expect(201);
+
+    const response = await request(app.getHttpServer())
+      .get('/statistics')
+      .expect(200);
+
+    expect(response.body).toEqual({
+      count: 1,
+      sum: 100,
+      avg: 100,
+      min: 100,
+      max: 100,
+    });
+  });
+
+  it('/transactions (DELETE) - should empty the database', async () => {
+    return request(app.getHttpServer()).delete('/transactions').expect(200);
   });
 });
